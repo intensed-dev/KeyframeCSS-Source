9652ce6f937399467de36fd454ba40f1c84735a1
Intensed <intensed01@gmail.com>
2026-06-22 17:32:16 +0200

    Load Lucide icons on DOMContentLoaded

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,8 @@
-
+document.addEventListener("DOMContentLoaded", () => {
+  const script = document.createElement("script");
+  script.src = "https://unpkg.com/lucide@latest";
+  script.onload = () => {
+    lucide.createIcons();
+  };
+  document.body.appendChild(script);
+});
